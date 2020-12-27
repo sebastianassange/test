2f6f488d310083a446fc8215dcfc18aa8b14af4e
sebastianassange <76642485+sebastianassange@users.noreply.github.com>
2020-12-27 17:41:19 +0800

    asdf

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,28 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-const index = () => {
+const index = (props) => {
   const [state, setState] = useState();
   useEffect(() => {
     axios.get("https://animermaid.herokuapp.com/anime/").then((res) => {
       setState(res.data.results);
     });
   }, []); // See Note 2
-  return <div>{state && JSON.stringify(state)}</div>;
+  return (
+    <div>
+      <div>{state && JSON.stringify(state)}</div>
+      <br></br>
+      <br></br>
+      <br></br>
+      {props.data && JSON.stringify(props.data)}
+    </div>
+  );
 };
 
 export default index;
+export async function getStaticProps(context) {
+  const res = await fetch("https://animermaid.herokuapp.com/anime/");
+  const data = await res.json();
+  return {
+    props: { data }, // will be passed to the page component as props
+  };
+}
