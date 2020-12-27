a5351abd39d5d1e163a257ce99648f29164a02bd
reilwaystation <reilwaystation@gmail.com>
2020-12-27 15:46:36 +0800

    initial

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,7 @@
-import Nav from '../components/nav'
+import React from "react";
 
-export default function IndexPage() {
-  return (
-    <div>
-      <Nav />
-      <div className="py-20">
-        <h1 className="text-5xl text-center text-gray-700 dark:text-gray-100">
-          Next.js + Tailwind CSS 2.0
-        </h1>
-      </div>
-    </div>
-  )
-}
+const index = () => {
+  return <div>Hello world</div>;
+};
+
+export default index;
